9f383b070443456fbe18461ff3cf684c96da6bf1
zhouhongzhan <zhz666521@163.com>
2020-07-25 02:07:07 +0800

    算法练习创库

---
+++ src/main/java/com/example/algorithmstest/AlgorithmstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithmstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmstestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithmstest/AlgorithmstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithmstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
